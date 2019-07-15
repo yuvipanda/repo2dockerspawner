dbd32b940f39ce66e28f3051a3233fb02b1f405a
mistamun <cyb3neto@gmail.com>
2019-07-15 10:32:01 +0100

    ask for repo / ref
    Repo and reference are not longer to be included in the config file, jupyter hub asks for input at the launch server page.

--- repo2dockerspawner/__init__.py
+++ repo2dockerspawner/__init__.py
@@ -23,7 +23,7 @@ async def subprocess_output(cmd, **kwargs):
 
     return stdout.decode(), stderr.decode()
 
-async def resolve_ref(repo_url, ref):
+async def resolve_ref(repo_url, requested_ref):
     """
     Return resolved commit hash for branch / tag.
 
@@ -40,7 +40,7 @@ async def resolve_ref(repo_url, ref):
     # exists
     all_refs = [l.split('\t') for l in stdout.strip().split('\n')]
     for hash, ref in all_refs:
-        if ref in (f'refs/heads/{ref}', f'refs/heads/{ref}'):
+        if requested_ref in hash or requested_ref in ref:
             return hash
 
     if stdout:
@@ -51,6 +51,30 @@ class Repo2DockerSpawner(DockerSpawner):
     # ThreadPool for talking to r2d
     _r2d_executor = None
 
+    def options_form(self, spawner):
+        return """
+        <label for="repo">Github repository to clone</label>
+        <input name="repo" placeholder="e.g. pytudes"></input>
+        <label for="ref">Reference to checkout</label>
+        <input name="ref" placeholder="master"></input>
+        """.format(
+            ref='master'
+        )
+    
+    def options_from_form(self, formdata):
+        options = {}
+        options['repo'] = ''
+        options['ref'] = ''
+        
+        repo_s = formdata.get('repo', [''])[0].strip()
+        if repo_s:
+            options['repo'] = repo_s
+            
+        ref_s = formdata.get('ref', [''])[0].strip()
+        if ref_s:
+            options['ref'] = ref_s
+        return options
+    
     def run_in_executor(self, func, *args):
         # FIXME: This shouldn't be used for anything other than r2d.build
         cls = self.__class__
@@ -60,7 +84,7 @@ class Repo2DockerSpawner(DockerSpawner):
         return IOLoop.current().run_in_executor(cls._r2d_executor, func, *args)
 
 
-    start_timeout = 10 * 60
+    start_timeout = 20 * 60
 
     # We don't want stopped containers hanging around
     remove = True
@@ -102,20 +126,26 @@ class Repo2DockerSpawner(DockerSpawner):
             return None
 
     async def start(self):
-        if self.repo is None:
-            raise ValueError("Repo2DockerSpawner.repo must be set")
-        resolved_ref = await resolve_ref(self.repo, self.ref)
-        repo_escaped = escape(self.repo, escape_char='-').lower()
+        repo_domain = 'https://github.com/mistamun/'
+        repo = self.user_options['repo']
+        resolved_ref = await resolve_ref(f'{repo_domain}{repo}', self.user_options['ref'])
+        repo_escaped = escape(f'{repo_domain}{repo}', escape_char='-').lower()
         image_spec = f'r2dspawner-{repo_escaped}:{resolved_ref}'
+        
+        self.log.debug(f'Repo: {repo_domain}{repo}')
+        self.log.debug(f'Ref: {self.user_options["ref"]}')
+        self.log.debug(f'Resolved ref: {resolved_ref}')
+        self.log.debug(f'Repo escaped: {repo_escaped}')
+        self.log.debug(f'Image spec: {image_spec}')
         
         image_info = await self.inspect_image(image_spec)
         if not image_info:
             self.log.info(f'Image {image_spec} not present, building...')
             r2d = Repo2Docker()
-            r2d.repo = self.repo
+            r2d.repo = f'{repo_domain}{repo}'
             r2d.ref = resolved_ref
             r2d.user_id = 1000
-            r2d.user_name = 'jovyan'
+            r2d.user_name = 'cfd'
 
             r2d.output_image_spec = image_spec
             r2d.initialize()
